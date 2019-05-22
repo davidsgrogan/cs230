4bb4fc6ff0c7387b5562b854a61698ac413d2ed0
David Grogan <dgrogan@chromium.org>
2019-05-21 23:15:47 -0700

    running the processing overnight

---
+++ find_20_speakers_stage_2.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 17 17:20:50 2019
+
+@author: dgrogan
+"""
+
+import glob
+import librosa
+import os
+import time
+from python_speech_features import mfcc, delta
+import pickle
+import numpy as np
+
+with open('durations_dict.txt', 'r') as content_file:
+    content = content_file.read()
+
+total_durations = eval(content)
+print(total_durations)
+
+sorted_by_duration = sorted(total_durations, key=lambda x: total_durations[x], reverse=True)
+
+top_20 = sorted_by_duration[0:21]
+# Throw out "the"
+top_20.remove("the");
+for title in top_20:
+    print(title, total_durations[title])
+    #print(title)
+
+duration_of_least = total_durations[top_20[-1]]
+
+print ("\nThe speaker with least data has %d seconds. So stop there for all speakers."
+       % duration_of_least)
+
+# nowitcanbetold has 68431 seconds. 68400 is exactly 19 hours, so just use that?
--- process_20_speakers.py
+++ process_20_speakers.py
@@ -14,36 +14,35 @@ from python_speech_features import mfcc, delta
 import pickle
 import numpy as np
 
-with open('durations_dict.txt', 'r') as content_file:
-    content = content_file.read()
-
-total_durations = eval(content)
-print(total_durations)
-
-sorted_by_duration = sorted(total_durations, key=lambda x: total_durations[x], reverse=True)
-
-top_20 = sorted_by_duration[0:21]
-# Throw out "the"
-top_20.remove("the");
-for title in top_20:
-    print(title, total_durations[title])
-    #print(title)
-
-duration_of_least = total_durations[top_20[-1]]
-
-print ("\nThe speaker with least data has %d seconds. So stop there for all speakers."
-       % duration_of_least)
-
-# nowitcanbetold has 68431 seconds. 68400 is exactly 19 hours, so just use that?
-
 def write_to_file(speaker_id, one_minute_of_examples, file_num, minute_num):
   np_array = np.array(one_minute_of_examples, dtype="float32")
   filename = 'data_np_save/speaker_%d_file_%d_cumulative_minute_%d' % (speaker_id, file_num, minute_num)
   print ("saving", filename)
   np.save(filename, np_array)
 
-speaker_id = 0
-top_20 = ["nowitcanbetold"]
+speaker_id = 1
+top_20 = [
+# "nowitcanbetold", I processed this one out of band and labeled it speaker 1
+"unspokensermons",
+"littlewomen",
+"artcookerymadeplaineasy1784",
+"historymathematics",
+"mysticalcityofgod1",
+"newtestament",
+"uncletomscabin",
+"seapower",
+"wildwales",
+"originofspecies",
+"mysteries",
+"1001nacht2",
+"rkopis",
+"innocentsabroad",
+"woutertjepieterse",
+"geschichtedespeloponnesischenkriegs",
+"vanityfair",
+"jerusalemrevelationsr",
+"worldenglishbible",
+]
 for file_prefix in top_20:
     speaker_id += 1
     this_speaker_glob = ('/usr/local/google/home/dgrogan/top20_mp3/%s_*.mp3'
@@ -84,6 +83,10 @@ for file_prefix in top_20:
             if (len(one_minute_of_examples) == 49 * 13 * 3 * 120):
                 write_to_file(speaker_id, one_minute_of_examples, file_num, minute_num)
                 minute_num += 1
+                if minute_num == 1106:
+                    break
                 one_minute_of_examples = []
+        if minute_num == 1106:
+            break
         print ("that file took %d seconds" % (time.time() - file_start_time))
     print ("that speaker took %d seconds" % (time.time() - speaker_start_time))
