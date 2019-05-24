b3c226d1ba0debbf99bd7a3bea52858c36fb5317
David Grogan <dgrogan@chromium.org>
2019-05-23 17:37:19 -0700

    We get 82% train, 78% dev with one hidden layer of 50
    That training is not very good, so we need to get a bigger network.

--- baseline_20_speakers.py
+++ baseline_20_speakers.py
@@ -21,8 +21,8 @@ os.environ["PATH"] += os.pathsep + '/Users/dgrogan/anaconda3/pkgs/graphviz-2.40.
 
 
 #%%
-np.random.seed(124568)
-random.seed(1899) # 85.8%, then 82% after restarting kernel, then 87% after resetting again
+np.random.seed(1268)
+random.seed(1889) # 85.8%, then 82% after restarting kernel, then 87% after resetting again
 # After setting sklearn seed, 84.7% then 85.2%, 84.8% <-- these were with batch size 128
 # Then using batch size of 256, finished in 55% of the time, but got only 84%
 # Then after changing batch size to 512, finished in 1/3 the time of 128, but only got 80%
@@ -57,12 +57,10 @@ start_time = time.time()
 # split our train/dev however we want as we go along.
 
 
-
-
 # Limit the amount of data we train on. We have 1047 minutes available per
 # speaker in the train/dev set on disk, so setting this number higher than that
 # is a no-op.
-MINUTES_PER_SPEAKER = 6
+MINUTES_PER_SPEAKER = 60
 # We have a max of 20 speakers but can change this to train on just a subset.
 NUM_SPEAKERS = 20
 
@@ -96,7 +94,7 @@ for speaker_num in range(1, NUM_SPEAKERS + 1):
         files_already_processed += 1
 
 # Cut down to only first derivative of MFCC
-train_dev_set = train_dev_set[:, 0:(49 * 13 * 2)]
+#train_dev_set = train_dev_set[:, 0:(49 * 13 * 2)]
 
 
 X_train, X_dev, y_train, y_dev = train_test_split(train_dev_set,
@@ -113,7 +111,8 @@ print ("%d seconds to load the data from disk" % (time.time() - start_time))
 model = tf.keras.Sequential()
 
 #hidden layers
-model.add(layers.Dense(5, activation='relu', input_dim=train_dev_set.shape[1]))
+model.add(layers.Dense(50, activation='relu', input_dim=train_dev_set.shape[1]))
+#model.add(layers.Dense(50, activation='relu'))
 model.add(layers.Dense(NUM_SPEAKERS, activation='softmax'))
 
 model.compile(optimizer=tf.train.AdamOptimizer(0.001),
@@ -126,7 +125,7 @@ print(model.summary())
 # I don't know where 140240419526080 in the picture came from
 
 start_time = time.time()
-history_object = model.fit(X_train, y_train, epochs=50, batch_size=256,
+history_object = model.fit(X_train, y_train, epochs=40, batch_size=1024,
                            verbose=2, shuffle=True, validation_data=(X_dev, y_dev))
 print ("%d seconds to train the model" % (time.time() - start_time))
 #%%
